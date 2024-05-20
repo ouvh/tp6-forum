92b1e4603327cf934890e09b7586434082279895
ouvh <oussamalaaroussiFCB@gmail.com>
2024-05-20 10:31:31 +0100

    prototype 1

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,13 @@ import 'firebase/functions';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
+    apiKey: "AIzaSyAOVc6q2ZM_c0HtavzQ1igBp-rLjhZiY7g",
+    authDomain: "forumapp-b502a.firebaseapp.com",
+    projectId: "forumapp-b502a",
+    storageBucket: "forumapp-b502a.appspot.com",
+    messagingSenderId: "1049661060851",
+    appId: "1:1049661060851:web:d2487423753dee47072986"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
