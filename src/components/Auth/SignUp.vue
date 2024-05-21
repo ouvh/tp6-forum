e3c5d45e6fe06bba1a7ca6a784b8e533c099d07c
unknown <malak012426@gmail.com>
2024-05-21 17:46:39 +0000

    updated sign up

--- src/components/Auth/SignUp.vue
+++ src/components/Auth/SignUp.vue
@@ -4,14 +4,36 @@
       <b-col md="6" lg="4">
         <b-card bg-variant="light" border-variant="success" class="signup-card">
           <b-form @submit.prevent="signUp">
+            <b-form-group label="Name">
+              <b-form-input v-model="name" type="text" required></b-form-input>
+            </b-form-group>
             <b-form-group label="Email">
-              <b-form-input v-model="email" type="email" required></b-form-input>
+              <b-form-input
+                v-model="email"
+                type="email"
+                required
+              ></b-form-input>
             </b-form-group>
             <b-form-group label="Password">
-              <b-form-input v-model="password" type="password" required></b-form-input>
+              <b-form-input
+                v-model="password"
+                type="password"
+                required
+              ></b-form-input>
             </b-form-group>
-            <b-button type="submit" variant="success" class="btn-block">Sign Up</b-button>
-            <b-button @click="goToLogin" variant="sucess" class="btn-block">Login</b-button>
+            <b-form-group label="Confirm Password">
+              <b-form-input
+                v-model="confirmPassword"
+                type="password"
+                required
+              ></b-form-input>
+            </b-form-group>
+            <b-button type="submit" variant="success" class="btn-block"
+              >Sign Up</b-button
+            >
+            <b-button @click="goToLogin" variant="link" class="btn-block"
+              >Login</b-button
+            >
           </b-form>
         </b-card>
       </b-col>
@@ -20,24 +42,43 @@
 </template>
 
 <script>
-import { auth } from '../../firebase';
+import { db,auth } from '../../firebase';
 
 export default {
   data() {
     return {
+      name: '',
       email: '',
-      password: ''
+      password: '',
+      confirmPassword: ''
     };
   },
   methods: {
-    async signUp() {
-      try {
+  async signUp() {
+      if (this.password !== this.confirmPassword) {
+          alert('Passwords do not match');
+          return;
+        }
+        try {
         await auth.createUserWithEmailAndPassword(this.email, this.password);
-        this.$router.push('/');
-      } catch (error) {
-        alert(error.message);
-      }
+
+          try {
+          await db.collection('users').doc(auth.currentUser.uid).set({
+            userid: auth.currentUser.uid,
+            name:this.name
+          },{ merge: true });
+          this.$router.push('/');
+        } catch (error) {``
+          alert(error.message);
+        }
+
+
+          this.$router.push('/');
+        } catch (error) {
+          alert(error.message);
+        }
     },
+
     goToLogin() {
       this.$router.push('/login');
     }
